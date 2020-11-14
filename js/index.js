5a2af5840b321967b8c2c9965a76a21c4d84a7e3
NaRaYeN <36738256+Narayen@users.noreply.github.com>
2020-11-14 23:44:23 +0530

    initial

---
+++ js/index.js
@@ -0,0 +1,55 @@
+$(document).ready(function(e) {
+	$("#download").click('submit',(function(e) {
+        e.preventDefault();
+
+        //Validations for url
+        function isUrlValid(url)
+        {
+            return /^(https?|s?ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(url);
+        }
+
+        if($('#url').val().length>0)
+        {
+            if($('#url').val().trim()=='')
+            {
+                alert('Enter URL');
+                $('#url').focus();
+                return;
+            }
+        }
+        else
+        {
+            alert('Enter URL');
+            $('#url').focus();
+            return;
+        }
+
+        var url = $('#url').val();
+
+        console.log(url);
+
+        if(isUrlValid(url))
+		{
+            console.log('ins');
+
+            $.ajax({
+                type: 'POST',
+                dataType:'json',
+                url: "../index.php",
+                data: { url: url},
+                processData: false,
+                contentType: false,
+
+                success: function(data)
+                {
+                    console.log(data);
+                }
+            });
+        }
+        else
+        {
+            console.log('Check URL again...');
+            alert('Check URL again...');
+        }
+    }));
+})
